34ea8364ea1165bcda99bfa79d9832c11f38b7bf
alana <alanjiraya77@gmail.com>
2017-07-15 02:00:24 -0500

    Se hicieron un monton de cosas

--- app/src/main/java/com/claresti/geturbines/MainActivity.java
+++ app/src/main/java/com/claresti/geturbines/MainActivity.java
@@ -1,25 +1,24 @@
 package com.claresti.geturbines;
 
 import android.content.Intent;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-    FloatingActionButton fab;
+    com.getbase.floatingactionbutton.FloatingActionButton send;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
-        fab=(FloatingActionButton)findViewById(R.id.process);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i= new Intent(MainActivity.this, Grafica.class);
-                startActivity(i);
-            }
-        });
+        //send=(com.getbase.floatingactionbutton.FloatingActionButton) findViewById(R.id.accion_Send);
+        //send.setOnClickListener(new View.OnClickListener() {
+            //@Override
+          //  public void onClick(View v) {
+          //      Intent i= new Intent(MainActivity.this, Grafica.class);
+            //    startActivity(i);
+        //    }
+       // });
     }
 }
