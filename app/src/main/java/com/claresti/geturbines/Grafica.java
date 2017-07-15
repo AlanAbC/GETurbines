888f5664154f8375f5219345a1fd162ff141bfe8
alana <alanjiraya77@gmail.com>
2017-07-15 00:01:38 -0500

    Se hicieron un monton de cosas

--- app/src/main/java/com/claresti/geturbines/Grafica.java
+++ app/src/main/java/com/claresti/geturbines/Grafica.java
@@ -1,23 +1,26 @@
 package com.claresti.geturbines;
 
 import android.content.Intent;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
-    FloatingActionButton fab;
+import com.getbase.floatingactionbutton.FloatingActionButton;
+
+public class Grafica extends AppCompatActivity {
+
+    FloatingActionButton tabla;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
-        setContentView(R.layout.activity_main);
-        fab=(FloatingActionButton)findViewById(R.id.process);
-        fab.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_grafica);
+
+        tabla=(FloatingActionButton)findViewById(R.id.accion_verTabla);
+        tabla.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i= new Intent(MainActivity.this, Grafica.class);
+                Intent i = new Intent(Grafica.this, Tabla.class);
                 startActivity(i);
             }
         });
