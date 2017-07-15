d9b6aadafccb092de60c751fbc3a87008bbecb6f
alana <alanjiraya77@gmail.com>
2017-07-15 00:32:23 -0500

    Se hicieron un monton de cosas

--- app/src/main/java/com/claresti/geturbines/Grafica.java
+++ app/src/main/java/com/claresti/geturbines/Grafica.java
@@ -8,19 +8,39 @@ import android.view.View;
 import com.getbase.floatingactionbutton.FloatingActionButton;
 
 public class Grafica extends AppCompatActivity {
-
+    //variables para el control de floatingButton
     FloatingActionButton tabla;
+    FloatingActionButton guardar;
+    FloatingActionButton home;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_grafica);
 
+
+        //Seccion de control de floatingButton
         tabla=(FloatingActionButton)findViewById(R.id.accion_verTabla);
         tabla.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(Grafica.this, Tabla.class);
+                startActivity(i);
+            }
+        });
+        guardar=(FloatingActionButton)findViewById(R.id.accion_guardar);
+        guardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Grafica.this, Tabla.class);
+                startActivity(i);
+            }
+        });
+        home=(FloatingActionButton)findViewById(R.id.accion_Home);
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Grafica.this, MainActivity.class);
                 startActivity(i);
             }
         });
--- app/src/main/java/com/claresti/geturbines/Tabla.java
+++ app/src/main/java/com/claresti/geturbines/Tabla.java
@@ -1,13 +1,47 @@
 package com.claresti.geturbines;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.getbase.floatingactionbutton.FloatingActionButton;
 
 public class Tabla extends AppCompatActivity {
-
+    //Variables para el control de floatingButton
+    FloatingActionButton tabla;
+    FloatingActionButton guardar;
+    FloatingActionButton home;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_tabla);
+
+        //Seccion de control de floatingButton
+        tabla=(FloatingActionButton)findViewById(R.id.accion_verGrafica);
+        tabla.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Tabla.this, Grafica.class);
+                startActivity(i);
+            }
+        });
+        guardar=(FloatingActionButton)findViewById(R.id.accion_Guardar);
+        guardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Tabla.this, Tabla.class);
+                startActivity(i);
+            }
+        });
+        home=(FloatingActionButton)findViewById(R.id.accion_Home);
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Tabla.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
