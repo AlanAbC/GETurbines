0fb44d5b683cc57e3cecfa7778124766a96aea89
alana <alanjiraya77@gmail.com>
2017-07-15 14:32:22 -0500

    Se agrego cambio de sistema

--- app/src/main/java/com/claresti/geturbines/Tabla.java
+++ app/src/main/java/com/claresti/geturbines/Tabla.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TableRow;
 import android.widget.TextView;
 
@@ -46,6 +47,8 @@ public class Tabla extends AppCompatActivity {
     TextView compt5;
     TextView turbp5;
     TextView turbt5;
+    ImageView change;
+    int flag=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -83,39 +86,93 @@ public class Tabla extends AppCompatActivity {
         compt5 = (TextView)findViewById(R.id.txt_compt5);
         turbp5 = (TextView)findViewById(R.id.txt_turbp5);
         turbt5 = (TextView)findViewById(R.id.txt_turbt5);
+        change=(ImageView)findViewById(R.id.change);
+
 
         elemento1.setText("1");
         air1.setText("7.198065547");
-        compp1.setText("74016371");
-        comptl.setText("490000.0");
-        turbp1.setText("102122.553502");
-        turbt1.setText("102122.726642");
+        compp1.setText("74016371 pas");
+        comptl.setText("490000.0 k");
+        turbp1.setText("102122.553502 pas");
+        turbt1.setText("102122.726642 k");
         elemento2.setText("2");
         air2.setText("6.4668370365");
-        compp2.setText("452.74016371");
-        compt2.setText("490000.0");
-        turbp2.setText("1154.98112201");
-        turbt2.setText("102122.726642");
+        compp2.setText("452.74016371 pas");
+        compt2.setText("490000.0 k");
+        turbp2.setText("1154.98112201 pas");
+        turbt2.setText("102122.726642 k");
         elemento3.setText("3");
         air3.setText("7.198065547");
-        compp3.setText("74016371");
-        compt3.setText("490000.0");
-        turbp3.setText("102122.553502");
-        turbt3.setText("102122.726642");
+        compp3.setText("74016371 pas");
+        compt3.setText("490000.0 k");
+        turbp3.setText("102122.553502 pas");
+        turbt3.setText("102122.726642 k");
         elemento4.setText("4");
         air4.setText("6.4668370365");
-        compp4.setText("452.74016371");
-        compt4.setText("490000.0");
-        turbp4.setText("1154.98112201");
-        turbt4.setText("102122.726642");
+        compp4.setText("452.74016371 pas");
+        compt4.setText("490000.0 k");
+        turbp4.setText("1154.98112201 pas");
+        turbt4.setText("102122.726642 k");
         elemento5.setText("5");
         air5.setText("7.198065547");
-        compp5.setText("74016371");
-        compt5.setText("490000.0");
-        turbp5.setText("102122.553502");
-        turbt5.setText("102122.726642");
+        compp5.setText("74016371 pas");
+        compt5.setText("490000.0 k");
+        turbp5.setText("102122.553502 pas");
+        turbt5.setText("102122.726642 k");
 
 
+        change.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(flag==0) {
+                    comptl.setText("881540.3℉");
+                    turbt1.setText("183361.2℉");
+                    compt2.setText("881540.3℉");
+                    turbt2.setText("183361.2℉");
+                    compt3.setText("881540.3℉");
+                    turbt3.setText("183361.2℉");
+                    compt4.setText("881540.3℉");
+                    turbt4.setText("183361.2℉");
+                    compt5.setText("881540.3℉");
+                    turbt5.setText("183361.2℉");
+                    compp1.setText("1.55700433659 psi");
+                    turbp1.setText("102122.553502 psi");
+                    compp2.setText("0.06566440912 psi");
+                    turbp2.setText("0.16751584906 psi");
+                    compp3.setText("1.55700433659 psi");
+                    turbp3.setText("102122.553502 psi");
+                    compp4.setText("0.06566440912 psi");
+                    turbp4.setText("0.16751584906 psi");
+                    compp5.setText("1.55700433659 psi");
+                    turbp5.setText("102122.553502 psi");
+
+                    flag++;
+                }else{
+                    comptl.setText("490000.0 k");
+                    turbt1.setText("102122.726642 k");
+                    compt2.setText("490000.0 k");
+                    turbt2.setText("102122.726642 k");
+                    compt3.setText("490000.0 k");
+                    turbt3.setText("102122.726642 k");
+                    compt4.setText("490000.0 k");
+                    turbt4.setText("102122.726642 k");
+                    compt5.setText("490000.0 k");
+                    turbt5.setText("102122.726642 k");
+                    compp1.setText("74016371 pas");
+                    turbp1.setText("102122.553502 pas");
+                    compp2.setText("452.74016371 pas");
+                    turbp2.setText("1154.98112201 pas");
+                    compp3.setText("74016371 pas");
+                    turbp3.setText("102122.553502 pas");
+                    compp4.setText("452.74016371 pas");
+                    turbp4.setText("1154.98112201 pas");
+                    compp5.setText("74016371 pas");
+                    turbp5.setText("102122.553502 pas");
+                    flag--;
+                }
+
+            }
+        });
 
                                         //Seccion de control de floatingButton
         tabla=(FloatingActionButton)findViewById(R.id.accion_verGrafica);
